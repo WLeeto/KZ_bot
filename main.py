d1038d122a3bc2be4240a860355f7cf8fb4709b0
WLeeto <leeto4848@gmail.com>
2022-11-01 20:06:19 +0600

    новая функция

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from telebot import types
 from courses import _get_courses, course_KZT
 
 KZ_course = 7.1
+storage = {}
 
 def read_file(path):
     with open(path, "r") as token:
@@ -17,6 +18,7 @@ def start(message):
     bot.send_message(message.chat.id, "Я умею быстро конвертировать валюты.\n"
                                       "Это очень помогает освоиться с новой валютой\n"
                                       "Примерно представить сколько что стоит в рублях")
+    init_storage(message.from_user.id)
     keep_going(message)
 
 def keep_going(message):
@@ -25,8 +27,9 @@ def keep_going(message):
     item_2 = types.KeyboardButton('Теньге в рубли')
     item_3 = types.KeyboardButton('Теньге в рубли по 7.1')
     item_4 = types.KeyboardButton('Разница между обменом по курсу и по 7.1 в теньге')
+    item_5 = types.KeyboardButton('По какому курсу мне поменяли ?')
 
-    markup.add(item_1, item_2, item_3, item_4)
+    markup.add(item_1, item_2, item_3, item_4, item_5)
     bot.send_message(message.chat.id, "Чем я могу помочь ?", reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
@@ -43,9 +46,50 @@ def input(message):
     elif message.text == 'Разница между обменом по курсу и по 7.1 в теньге':
         bot.send_message(message.chat.id, "Считаем разницу по обмену в теньге. Введи сумму в рублях:")
         bot.register_next_step_handler(message, count_kzt)
+    elif message.text == 'По какому курсу мне поменяли ?':
+        bot.send_message(message.chat.id, "Давай посчитаем по какому курсу тебе поменяли рубли в теньге.\n"
+                                          "Сколько теньге выдали ?")
+        bot.register_next_step_handler(message, what_course)
     else:
         bot.send_message(message.chat.id, "Такой команды я не знаю")
         keep_going(message)
+
+def init_storage(user_id):
+  storage[user_id] = dict(KZT=None, RUB=None)
+
+def store_number(user_id, key, value):
+  storage[user_id][key] = dict(value=value)
+
+def get_number(user_id, key):
+  return storage[user_id][key].get('value')
+
+def what_course(message):
+    '''
+    Считаем курс обмена
+    '''
+    KZT = _replace(message.text)
+    try:
+        float(KZT)
+        store_number(message.from_user.id, "KZT", KZT)
+        bot.send_message(message.chat.id, 'Сколько RUB списалось ?')
+        bot.register_next_step_handler(message, what_course_2)
+    except Exception:
+        bot.send_message(message.chat.id, 'Похоже вы ввели не число =\\')
+        keep_going(message)
+
+
+def what_course_2(message):
+    RUB = _replace(message.text)
+    try:
+        float(RUB)
+        course = float(get_number(message.from_user.id, "KZT")) / float(RUB)
+        course = round(course, 1)
+        bot.send_message(message.chat.id, f'Похоже вы обменяли по курсу {course}')
+        keep_going(message)
+    except Exception:
+        bot.send_message(message.chat.id, 'Похоже вы ввели не число =\\')
+        keep_going(message)
+
 
 def convert_rub_to_kz_cb(message):
     '''
